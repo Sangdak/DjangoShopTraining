772a0f770751850c3b152a5a4be71f1256d83252
Sangdak <82779027+Sangdak@users.noreply.github.com>
2024-05-11 09:44:35 +1000

    Add login form.

--- store/users/views.py
+++ store/users/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from users.forms import UserLoginForm
+from users.models import User
+
 
 def login(request):
-    return render(request, 'users/login.html')
+    context = {
+        'form': UserLoginForm(),
+    }
+    return render(request, 'users/login.html', context)
 
 
 def registration(request):
