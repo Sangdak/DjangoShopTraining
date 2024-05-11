6944a226cdf89d24b8722e1b6a3916ddd6157bc2
Sangdak <82779027+Sangdak@users.noreply.github.com>
2024-05-11 10:35:26 +1000

    Setup login feature backend.

--- store/users/views.py
+++ store/users/views.py
@@ -1,12 +1,26 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from users.forms import UserLoginForm
 from users.models import User
 
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
     context = {
-        'form': UserLoginForm(),
+        'form': form,
     }
     return render(request, 'users/login.html', context)
 
