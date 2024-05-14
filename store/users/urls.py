466d131733ba28269109d0719c015cfaacffe146
Sangdak <82779027+Sangdak@users.noreply.github.com>
2024-05-15 03:56:21 +1000

    Add links to navbar.

--- store/users/urls.py
+++ store/users/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from users.views import login, profile, registration
+from users.views import login, logout, profile, registration
 
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
-    path('registration/', registration, name='registration'),
+    path('logout/', logout, name='logout'),
     path('profile/', profile, name='profile'),
+    path('registration/', registration, name='registration'),
+
 ]
