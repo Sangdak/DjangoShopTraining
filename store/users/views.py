6a8158f0ec71db96bed6d905fe25a8727ecd0807
Sangdak <82779027+Sangdak@users.noreply.github.com>
2024-05-15 03:55:37 +1000

    Add logout feature.

--- store/users/views.py
+++ store/users/views.py
@@ -54,3 +54,8 @@ def profile(request):
         'form': form,
     }
     return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
