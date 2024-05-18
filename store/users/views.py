3f3842aaf6505af0e9ae829f43dd1712e2d7067a
Sangdak <82779027+Sangdak@users.noreply.github.com>
2024-05-19 07:02:09 +1000

    Add access decorators.

--- store/users/views.py
+++ store/users/views.py
@@ -1,4 +1,5 @@
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -40,6 +41,7 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
