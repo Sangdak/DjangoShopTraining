0f25443104a1b8107bd8f96e8f0912292b39d1fd
Sangdak <82779027+Sangdak@users.noreply.github.com>
2024-05-24 18:05:17 +1000

    Made refactoring for login & Logout func to CBV.

--- store/users/urls.py
+++ store/users/urls.py
@@ -1,13 +1,14 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from users.views import login, logout, UserProfileView, UserRegistrationView
+from users.views import logout, UserLoginView, UserProfileView, UserRegistrationView
 
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', login, name='login'),
-    path('logout/', logout, name='logout'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('registration/', UserRegistrationView.as_view(), name='registration'),
 ]
--- store/users/views.py
+++ store/users/views.py
@@ -1,5 +1,6 @@
 from django.contrib import auth, messages
 from django.contrib.auth import get_user_model
+from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -11,22 +12,27 @@ from users.models import User
 from users.forms import UserLoginForm, UserProfileForm, UserRegistrationForm
 
 
-def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-    else:
-        form = UserLoginForm()
-    context = {
-        'form': form,
-    }
-    return render(request, 'users/login.html', context)
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
+
+
+# def login(request):
+#     if request.method == 'POST':
+#         form = UserLoginForm(data=request.POST)
+#         if form.is_valid():
+#             username = request.POST['username']
+#             password = request.POST['password']
+#             user = auth.authenticate(username=username, password=password)
+#             if user:
+#                 auth.login(request, user)
+#                 return HttpResponseRedirect(reverse('index'))
+#     else:
+#         form = UserLoginForm()
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, 'users/login.html', context)
 
 
 class UserRegistrationView(CreateView):
